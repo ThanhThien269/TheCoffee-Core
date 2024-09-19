1bf6e8ab4ba04e88997bd5428baa3340a1136dec
thanhhuy <buithanhhuy104@gmail.com>
2024-09-19 22:48:15 +0700

    init

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "antd";
+
+export default function Home() {
+  return (
+    <div>
+      hello world
+      <div>
+        <Button type="primary">Button</Button>
+      </div>
+    </div>
+  );
+}
