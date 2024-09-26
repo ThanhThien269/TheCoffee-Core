62300037a17e42642b47c691e2633e4158c6fd85
thanhhuy <buithanhhuy104@gmail.com>
2024-09-26 17:17:59 +0700

    CRUD user

---
+++ coffee-core/src/helper/util.ts
@@ -0,0 +1,10 @@
+const bcryptjs = require('bcryptjs');
+const saltRounds = 10;
+
+export const hashPasswordHelper = async (plainPassword: string) => {
+  try {
+    return await bcryptjs.hash(plainPassword, saltRounds);
+  } catch (error) {
+    console.log(error);
+  }
+};
